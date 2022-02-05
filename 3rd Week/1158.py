bd546325bbe5574ce4b388d536ec5f39a48b806e
HIKI <97379189+otakuhiki@users.noreply.github.com>
2022-02-05 23:51:38 +0900

    Add files via upload

--- 3rd Week/1158.py
+++ 3rd Week/1158.py
@@ -1,1 +1,24 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+
+n, k = map(int, input().split())
+q = deque([i for i in range(1,n+1)])
+#print(q)
+target = 0
+cnt=1
+output = []
+while(cnt!= n+1):
+    #target = target + k
+    q.rotate(-(k-1))
+    print(q)
+    out = q.popleft()
+    output.append(out)
+    cnt+=1
+    print('-제거된 사람: ', out, '\n', '-제거 후: ' , q)
+
+print('<', end='')
+print(*output, sep=', ', end='')
+print('>')
 
