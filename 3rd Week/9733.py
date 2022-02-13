30e63d722e11ec62707cdba05d527bb161f779fe
HIKI <97379189+otakuhiki@users.noreply.github.com>
2022-02-13 17:36:25 +0900

    입력 해결 못해서 참고용

--- 3rd Week/9733.py
+++ 3rd Week/9733.py
@@ -1,21 +1,19 @@
-# 틀렸는데,,,모르겠네
 import sys
-input = sys.stdin.readline
-
-dayWork = list(input().split())
-work = { 'Re': 0, 'Pt': 0, 'Cc':0, 'Ea':0, 'Tb':0, 'Cm':0, 'Ex':0}
-#print(dayWork)
-cnt=0
-for i in range(0, len(dayWork)):
-    for key, value in work.items():
-        if dayWork[i] in key:
-            work[key]=value+1
-total = len(dayWork)
-
-#print(work, total)
-
-for key, value in work.items():
-    print(key, value , format(value/total, '.2f'))
-print('Total', total, '1.00')
-
-#input: Cc Pt Pt Re Tb Re Cm Cm Re Pt Pt Re Ea Ea Pt Pt Pt Re Re Cb Cb Pt Pt Cb
+work= ['Re', 'Pt', 'Cc', 'Ea', 'Tb', 'Cm', 'Ex']
+bee = {}
+for i in work:
+    bee[i] = 0
+ 
+cnt = 0
+lines = sys.stdin.readlines()
+for line in lines:
+    txt = list(line.split())
+    for i in txt:
+        if i in lst:
+            bee[i] = bee.get(i, 0) + 1
+        cnt += 1
+ 
+for i in bee:
+    print(i, bee.get(i), '{:.2f}'.format(bee.get(i)/cnt))
+ 
+print('Total', cnt, '1.00')
