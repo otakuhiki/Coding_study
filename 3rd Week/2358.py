0b349277926f1cd865b4868dfc6d032adb7ebd89
HIKI <97379189+otakuhiki@users.noreply.github.com>
2022-02-13 17:10:36 +0900

    Update 2358.py
    재풀이

--- 3rd Week/2358.py
+++ 3rd Week/2358.py
@@ -1,24 +1,34 @@
-#시간초과 : 완전탐색 이외의 방법 생각해보기
-
 import sys
 input = sys.stdin.readline
 
-#완전 탐색 -> 두 점의 x좌표나 y좌표가 같으면 x, y축에 평행 한 직선임
+#두 점의 x좌표나 y좌표가 같으면 x, y축에 평행 한 직선
+#(2,1)(2,2)와 (2,1)(2,3)은 같은 직선
 
 n = int(input())
-point=[]
+pointX={}
+pointY={}
+cnt = 0
+
+# key:각 좌표, value:갯수
+#x,y좌표를 따로 dict의 key로 넣어서 해당 key가 있을 경우 value를 더해줌 
+
 for i in range(0,n):
-    point.append(list(map(int, input().split())))
+    x, y = map(int, input().split())
+    if x in pointX:     
+        pointX[x]+=1
+    else:
+        pointX[x]=1
+    if y in pointY:
+        pointY[y]+=1
+    else:
+        pointY[y]=1
 
-print(point)
-
-#반복 횟수는 nC2 
-cnt = (n * n-1)/2
-line = 0
-
-for i in range(0, n):
-    for j in range(i+1, n):
-        if(point[i][0] == point[j][0] or point[i][1] == point[j][1]):
-            line +=1
+#좌표 갯수가 2개이상인 key를 세줌
+for k, v in pointX.items():
+    if pointX[k] != 1:
+        cnt+=1
+for k, v in pointY.items():
+    if pointY[k] != 1:
+        cnt+=1
         
-print(line)
+print(cnt)
