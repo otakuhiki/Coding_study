547ae64a1086abf8bf7d8812e2cc2716bb06e8e0
HIKI <97379189+otakuhiki@users.noreply.github.com>
2022-02-23 01:52:17 +0900

    Update 12034.py
    스터디 리뷰 코드 참조

--- 4th Week/12034.py
+++ 4th Week/12034.py
@@ -1,6 +1,24 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
+out= []
 
-for i in range(0,n):
+for _ in range(n):
+    num = int(input())
+    # 금액은 오름차순으로 입력됨
+    price = list(map(int, input().split()))
+    outCase=[]
+    
+    while(len(price)):
+        # 125%에 해결하는 금액이 있으면 pop()을 하여 출력 배열에 삽입
+        if price[0]*4/3 in price:
+            price.pop(price.index(price[0]*4/3))
+            outCase.append(price.pop(0))
+        # 할인 금액만 찾으면 되므로 나머지는 그냥 pop()
+        else: price.pop(0)
+    # Case별로 출력해야 함
+    out.append(outCase)
+
+for i in range(n):
+    print("Case #{}:".format(i+1,),end=" ") 
+    for j in range(len(out[i])):
+        print(out[i][j],end=" ")
+    print()
